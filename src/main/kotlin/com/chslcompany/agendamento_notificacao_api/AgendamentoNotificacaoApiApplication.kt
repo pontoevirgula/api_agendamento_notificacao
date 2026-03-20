9c06f0253b089396d294682447dbbda89ecc4677
Carlos Lima <inv.clima@ifood3rd.com>
2026-03-19 22:25:19 -0300

    commit inicial

---
+++ src/main/kotlin/com/chslcompany/agendamento_notificacao_api/AgendamentoNotificacaoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.chslcompany.agendamento_notificacao_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgendamentoNotificacaoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgendamentoNotificacaoApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/chslcompany/agendamento_notificacao_api/AgendamentoNotificacaoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chslcompany.agendamento_notificacao_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgendamentoNotificacaoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
