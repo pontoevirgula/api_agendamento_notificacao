cdd9bc8c6c377f483931a285c04f8e274a8229d5
Carlos Lima <inv.clima@ifood3rd.com>
2026-03-20 17:21:52 -0300

    add metodos de agendamento

---
+++ src/main/kotlin/com/chslcompany/agendamento_notificacao_api/business/AgendamentoService.kt
@@ -0,0 +1,38 @@
+package com.chslcompany.agendamento_notificacao_api.business
+
+import com.chslcompany.agendamento_notificacao_api.business.mapper.AgendamentoMapper
+import com.chslcompany.agendamento_notificacao_api.controller.dto.`in`.AgendamentoRecord
+import com.chslcompany.agendamento_notificacao_api.controller.dto.out.AgendamentoRecordOut
+import com.chslcompany.agendamento_notificacao_api.infrastructure.repositories.AgendamentoRepository
+import lombok.RequiredArgsConstructor
+import org.springframework.stereotype.Service
+
+
+@Service
+class AgendamentoService (
+   private val  repository : AgendamentoRepository,
+   private val  agendamentoMapper: AgendamentoMapper
+) {
+
+    fun gravarAgendamento(agendamentoRecord: AgendamentoRecord): AgendamentoRecordOut {
+        return agendamentoMapper.paraOut(
+            repository.save(agendamentoMapper.paraEntity(agendamentoRecord))
+        )
+    }
+
+//    fun buscarAgendamentosPorId(id: Long): AgendamentoRecordOut {
+//        return agendamentoMapper.paraOut(
+//            repository.findById(id)
+//                .orElseThrow { NotFoundException("Id não encontrado") }
+//        )
+//    }
+
+//    fun cancelarAgendamento(id: Long) {
+//        val agendamento = repository.findById(id)
+//            .orElseThrow { NotFoundException("Id não encontrado") }
+//        repository.save(
+//            agendamentoMapper.paraEntityCancelamento(agendamento)
+//        )
+//    }
+
+}
---
+++ src/main/kotlin/com/chslcompany/agendamento_notificacao_api/infrastructure/entities/StatusNotificacaoEnum.kt
@@ -0,0 +1,7 @@
+package com.chslcompany.agendamento_notificacao_api.infrastructure.entities
+
+enum class StatusNotificacaoEnum {
+    AGENDADO,
+    ENVIADO,
+    CANCELADO
+}
